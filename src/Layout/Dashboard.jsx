d54c9550c7b06465ae3114c60104eb60a109c29f
MuhaimanAlamMazumder <muhaimennabil@gmail.com>
2025-09-10 20:50:38 +0600

    Describe what you changed here

---
+++ src/Layout/Dashboard.jsx
@@ -0,0 +1,85 @@
+import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
+
+
+const Dashboard = () => {
+
+    const [cart] = useCart();
+    return (
+        <div className="flex">
+
+            <div className="w-64 min-h-screen bg-orange-400">
+
+                <ul className="menu p-4">
+                    <li>
+                        
+                        <NavLink to="/dashboard/userHome">
+                        <FaHome></FaHome>
+                        User Home</NavLink>
+                        
+                        </li>
+                        <li>
+                        
+                        <NavLink to="/dashboard/reservation">
+                        <FaCalendar></FaCalendar>
+                        Reservation</NavLink>
+                        
+                        </li>
+                        <li>
+                        
+                        <NavLink to="/dashboard/cart">
+                        <FaShoppingCart></FaShoppingCart>
+                        My Cart ({cart.length})</NavLink>
+                        
+                        </li>
+
+                        <li>
+                        
+                        <NavLink to="/dashboard/review">
+                        <FaAd></FaAd>
+                        Add a review</NavLink>
+                        
+                        </li>
+
+                        <li>
+                        
+                        <NavLink to="/dashboard/bookings">
+                        <FaList></FaList>
+                        My bookings</NavLink>
+                        
+                        </li>
+                        <div className="divider"></div>
+
+                        <li>
+                        
+                        <NavLink to="/">
+                        <FaHome></FaHome>
+                         Home</NavLink>
+                        
+                        </li>
+
+                        <li>
+                        
+                        <NavLink to="/order/salad">
+                        <FaSearch></FaSearch>
+                        Search</NavLink>
+                        
+                        </li>
+                </ul>
+
+
+            </div>
+
+            <div className="flex-1 p-8">
+
+                <Outlet></Outlet>
+
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Dashboard;
